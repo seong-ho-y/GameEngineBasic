bf84d39012e340eff521737cc91aec4a680b4728
wonkii <wkddnjswns0368@khu.ac.kr>
2025-11-05 23:19:19 +0900

    Update_AimandChargeShot

--- Source/GameEngineBasic/Components/public/ShooterComp.h
+++ Source/GameEngineBasic/Components/public/ShooterComp.h
@@ -27,6 +27,12 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shoot|Data")
 	float FireRate = 0.1f;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shoot|Data")
+	float PendingDamage = 5.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shoot|Data")
+	float PendingScale = 1.f;
+
 	// 탄약
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Shoot|Data")
 	int32 MaxAmmo;
@@ -46,11 +52,9 @@ public:
 	USoundBase* FireSound;
 
 protected:
-	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable, Category = "Combat")
@@ -76,4 +80,7 @@ private:
 
 	// 쿨다운이 끝나면 bIsReadyToFire를 true로 되돌리는 함수
 	void ResetFireReady();
+
+public:
+	
 };
