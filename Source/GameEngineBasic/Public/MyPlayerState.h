59ccca488405159d22b7aac543d041627a571af4
wonkii <wkddnjswns0368@khu.ac.kr>
2025-12-02 22:41:02 +0900

    PlayerDie_Save_AbilityUI

--- Source/GameEngineBasic/Public/MyPlayerState.h
+++ Source/GameEngineBasic/Public/MyPlayerState.h
@@ -7,6 +7,30 @@
 
 class UInventoryComponent;
 
+USTRUCT(BlueprintType)
+struct FUnlockStatus
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TSet<FName> UnlockedWeapons;
+
+	UPROPERTY()
+	TSet<FName> UnlockedParts;
+};
+
+USTRUCT(BlueprintType)
+struct FAbilityUnlockStatus
+{
+	GENERATED_BODY()
+
+	UPROPERTY() bool bCanSprint = false;
+	UPROPERTY() bool bCanDash = false;
+	UPROPERTY() bool bCanFly = false;
+	UPROPERTY() bool bCanShield = false;
+	UPROPERTY() bool bCanBoost = false;
+};
+
 UCLASS()
 class GAMEENGINEBASIC_API AMyPlayerState : public APlayerState
 {
@@ -15,9 +39,39 @@ class GAMEENGINEBASIC_API AMyPlayerState : public APlayerState
 public:
 	AMyPlayerState();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Unlock")
+	FUnlockStatus UnlockStatus;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FAbilityUnlockStatus AbilityStatus;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UInventoryComponent* Inventory;
 
 protected:
 	virtual void BeginPlay() override;
+
+public:
+	// 기능: 무기 / 파츠 언락
+	UFUNCTION()
+	void UnlockWeapon(FName WeaponRowName);
+
+	UFUNCTION()
+	void UnlockPart(FName PartRowName);
+
+	// 기능: 이미 해금했는지 검사
+	UFUNCTION(BlueprintCallable)
+	bool IsWeaponUnlocked(FName WeaponRowName) const;
+
+	UFUNCTION(BlueprintCallable)
+	bool IsPartUnlocked(FName PartRowName) const;
+
+	// 기능: 능력 해금
+	UFUNCTION()
+	void UnlockAbility(EAbilityType Ability);
+	
+	// 기능: 능력 사용 가능 여부 검사
+	UFUNCTION(BlueprintCallable)
+	bool CanUseAbility(EAbilityType Ability) const;
+
 };
